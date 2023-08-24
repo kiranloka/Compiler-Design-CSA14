66ccc005bcb7845a73a7978499a90900ebb89c9b
kiranloka <112564858+kiranloka@users.noreply.github.com>
2023-08-24 09:00:02 +0530

    Output

--- FOLLOW.c
+++ FOLLOW.c
@@ -1,63 +1,80 @@
-#include<stdio.h>
-#include<string.h>
-int n,m=0,p,i=0,j=0;
-char a[10][10],f[10];
-void follow(char c);
-void first(char c);
-int main()
-{
- int i,choice;
- char c,ch;
- printf("Enter the no.of productions:");
- scanf("%d",&n);
- printf("Enter the productions(epsilon=@):\n");
- for(i=0;i<n;i++)
-  scanf("%s%c",a[i],&ch);
-do
- {
-  m=0;
-  printf("Enter the element whose FOLLOW is to be found:");
-scanf("%c",&c);
-  follow(c);
-  printf("FOLLOW(%c) = { ",c);
-  for(i=0;i<m;i++)
-   printf("%c ",f[i]);
-  printf(" }\n");
-  printf("Do you want to continue(0/1)?");
-  scanf("%d%c",&choice,&ch);
- }
- while(choice==1);
-}
-void follow(char c)
-{
- if(a[0][0]==c)f[m++]='e';
- for(i=0;i<n;i++)
- {
-  for(j=2;j<strlen(a[i]);j++)
-  {
-   if(a[i][j]==c)
-   {
-    if(a[i][j+1]!='\0')first(a[i][j+1]);
-    if(a[i][j+1]=='\0'&&c!=a[i][0])
-    follow(a[i][0]);
-   }
-  }
- }
-}
-void first(char c)
-{
-    int k;
-    if(!(isupper(c)))f[m++]=c;
-    for(k=0;k<n;k++)
-{
-    if(a[k][0]==c)
-     {
-        if(a[k][2]=='@')
-  		follow(a[i][0]);
-        else if(islower(a[k][2]))
-		f[m++]=a[k][2];
-        else 
-		first(a[k][2]);
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+#define MAX_NON_TERMINALS 10
+#define MAX_TERMINALS 10
+
+// Production rules
+char productions[MAX_NON_TERMINALS][20];
+int numProductions = 0;
+
+// Follow sets
+char followSets[MAX_NON_TERMINALS][MAX_TERMINALS];
+int numFollowSets = 0;
+
+// Function to add a character to a set
+void addToSet(char *set, char c) {
+    if (strchr(set, c) == NULL) {
+        set[strlen(set)] = c;
     }
 }
+
+// Function to calculate FOLLOW set
+void calculateFollowSet(char nonTerminal) {
+    char set[MAX_TERMINALS] = "";
+    if (nonTerminal == productions[0][0]) {
+        addToSet(set, '$'); // $ for the start symbol
+    }
+
+    for (int i = 0; i < numProductions; i++) {
+        char *rhs = strchr(productions[i], '>');
+        if (rhs != NULL) {
+            rhs++; // Move to the right-hand side of the production
+            while (*rhs != '\0') {
+                if (*rhs == nonTerminal) {
+                    if (*(rhs + 1) == '\0') {
+                        calculateFollowSet(productions[i][0]);
+                        addToSet(set, '\0');
+                    } else {
+                        char *firstSet = strchr(followSets[nonTerminal - 'A'], '\0');
+                        addToSet(set, *(firstSet + 1));
+                    }
+                }
+                rhs++;
+            }
+        }
+    }
+
+    strcpy(followSets[nonTerminal - 'A'], set);
+    numFollowSets++;
+}
+
+int main() {
+    printf("Enter the number of productions: ");
+    scanf("%d", &numProductions);
+    getchar(); // Consume the newline character
+
+    printf("Enter the productions (one per line):\n");
+    for (int i = 0; i < numProductions; i++) {
+        fgets(productions[i], sizeof(productions[i]), stdin);
+    }
+
+    // Initialize followSets
+    for (int i = 0; i < MAX_NON_TERMINALS; i++) {
+        strcpy(followSets[i], "");
+    }
+
+    // Calculate FOLLOW sets
+    for (int i = 0; i < numProductions; i++) {
+        calculateFollowSet(productions[i][0]);
+    }
+
+    // Print the calculated FOLLOW sets
+    printf("FOLLOW sets:\n");
+    for (int i = 0; i < numFollowSets; i++) {
+        printf("FOLLOW(%c) = {%s}\n", 'A' + i, followSets[i]);
+    }
+
+    return 0;
 }
