20007197e0b99760fbad98d576863ab5978f9d9e
kiranloka <112564858+kiranloka@users.noreply.github.com>
2023-08-24 08:23:29 +0530

    Create Whitespaces_newline.c

---
+++ Whitespaces_newline.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    char input[1000];
+    printf("Enter a string:\n");
+    fgets(input, sizeof(input), stdin);
+
+    int whitespaceCount = 0;
+    int newlineCount = 0;
+
+    for (int i = 0; input[i] != '\0'; i++) {
+        if (input[i] == ' ') {
+            whitespaceCount++;
+        } else if (input[i] == '\n') {
+            newlineCount++;
+        }
+    }
+
+    printf("Number of whitespaces: %d\n", whitespaceCount);
+    printf("Number of newline characters: %d\n", newlineCount);
+
+    return 0;
+}
